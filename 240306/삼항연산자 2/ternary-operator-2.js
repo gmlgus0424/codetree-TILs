058f76a7911c6fd55a20bd34b41406e599f8510c
choi hee jin <81346229+gmlgus0424@users.noreply.github.com>
2024-03-06 17:28:01 +0900

    Title: 삼항연산자 2, Time: 231ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240306/삼항연산자 2/ternary-operator-2.js
@@ -0,0 +1,11 @@
+// 변수 선언 및 입력
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+
+
+let result = a ===1 ? "t" : "f";
+
+// 출력
+console.log(result);
