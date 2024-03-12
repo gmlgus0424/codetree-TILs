348b0b062eb76d8bb0a6a38fd2d43939bf747c98
choi hee jin <81346229+gmlgus0424@users.noreply.github.com>
2024-03-12 14:39:50 +0900

    Title: 문자열의 길이 출력하기, Time: 180ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240312/문자열의 길이 출력하기/print-string's-range.js
+++ 240312/문자열의 길이 출력하기/print-string's-range.js
@@ -1,10 +1,10 @@
 // 변수 선언 및 입력
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim();
-let input2= fs.readFileSync(0).toString().trim();
 
+let [str1, str2]= input;
 
+let len1 = str1.length;
+let len2 = str2.length;
 
-
-// 문자열을 두 번 출력합니다.
-console.log(input.length + input2.length-1);
+console.log(len1 + len2);
