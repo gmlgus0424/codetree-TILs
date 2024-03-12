e3e321f6bcdd1f28c508ddbe96d67fcf112edb94
choi hee jin <81346229+gmlgus0424@users.noreply.github.com>
2024-03-12 14:50:13 +0900

    Title: 더 긴 문자열, Time: 164ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240312/더 긴 문자열/longer-string.js
@@ -0,0 +1,16 @@
+// 변수 선언 및 입력
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("");
+
+let [str1, str2] = input;
+
+let len1= str1.length
+let len2= str2.length
+
+if (len1 > len2) {
+    console.log(str1 + " " + len1);
+} else if (len1 < len2) {
+    console.log(str2 + " " + len2);
+} else {
+    console.log("same");
+}
