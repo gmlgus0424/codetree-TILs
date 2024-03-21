b4fe2a878385c9f23cadb6cceab8bee8f887c341
choi hee jin <81346229+gmlgus0424@users.noreply.github.com>
2024-03-21 17:02:28 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 204ms, Memory: 14MB, Status: Passed - CodeTree

--- 240321/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.js
+++ 240321/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
-let n = Number(fs.readFileSync(0).toString().trim());
+let input = fs.readFileSync(0).toString().trim().split("\n");
 let n = Number(input[0]);
 let arr = input[1].split(" ").map(Number);
 
 
-function aaa(n) {
+
+
+function aaa(a) {
     if (a === 0) return arr[0];
 
     return Math.max(aaa(a - 1), arr[a]);
