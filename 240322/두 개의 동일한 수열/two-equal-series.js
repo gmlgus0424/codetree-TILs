986db6d0536fd34eada86964f758dc73cdf0d69a
choi hee jin <81346229+gmlgus0424@users.noreply.github.com>
2024-03-22 16:57:45 +0900

    Title: 두 개의 동일한 수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240322/두 개의 동일한 수열/two-equal-series.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+// 변수 선언 및 입력 
+const n = Number(input[0]);
+const a = input[1].trim().split(' ').map(Number);
+const b = input[2].trim().split(' ').map(Number);
+
+
+function aaa() {
+
+//주어진 조건 
+    for (let i = 0; i < n; i++) {
+        if (a[i] !== b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// 정렬
+a.sort((a, b) => a - b);
+b.sort((a, b) => a - b);
+
+
+if (equal()) {
+    console.log("Yes");
+} else {
+    console.log("No");
+}
